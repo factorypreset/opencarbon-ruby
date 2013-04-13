24697b68ce4aa1b2a9a9d0aae1283e86a9cdf4d1
Bo Daley <bo@factorypreset.com>
2013-04-13 11:01:16 -0400

    some more datatypes

--- lib/opencarbon/datatypes.rb
+++ lib/opencarbon/datatypes.rb
@@ -4,19 +4,108 @@ module OpenCarbon
 
   module DataTypes
   
+    # Generic datatype for numeric data with a unit of measure
     class NumericFieldWithUnits
 
-      attr_accessor :name, :value, :units
+      attr_accessor :node, :name, :value, :units
 
+      # Constructor
+      #
+      # param node [JSON element]
       def initialize(node)
-        self.name = node.keys.first
-        self.value = node[self.name]['numericAmount']
-        self.units = node[self.name]['unitOfMeasurement']
+        self.node = node
+        self.name = self.node.keys.first
+        self.set_values()
+      end
+
+      def set_values
+        self.value = self.node[self.name]['numericAmount']
+        self.units = self.node[self.name]['unitOfMeasurement']
       end
 
       def pretty
         "#{self.value} #{self.units}"
       end
+
+    end
+
+    class Baseline < NumericFieldWithUnits
+
+      attr_accessor :year
+
+      def set_values
+        self.year = self.node[self.name]['year']
+        super
+      end
+
+    end
+
+    class Total < NumericFieldWithUnits
+    end
+
+    class Scope < NumericFieldWithUnits
+    end
+
+    # Generic collection datatype
+    class Collection
+
+      attr_accessor :node, :collection
+
+      def initialize(node, base_element)
+        self.node = node
+        self.collection = []
+
+        self.process(base_element)
+      end
+
+      def process(base_element)
+        items = self.node[base_element]
+
+        items.keys.each do |key|
+          new_node = {}
+          new_node[key] = self.node[base_element][key]
+          self << new_node
+        end
+      end
+
+      # Append to collection
+      #
+      # param node [JSON element]
+      def <<(node)
+        scope = Scope.new(node)
+        self.collection << scope
+      end
+
+      # Get the named element from collection
+      #
+      # param name [String]
+      def find_by_name(name)
+        for item in self.collection
+          return item if item.name == name
+        end
+
+        # didn't find item in collection, return nil
+        return nil
+      end
+
+    end
+
+    # A collection of Scopes
+    class Scopes < Collection
+
+      # Constructor
+      #
+      # param node [JSON element]
+      def initialize(node, base_element='scopes')
+        self.node = node
+        self.collection = []
+        self.process(base_element)
+      end
+
+    end
+
+    # Source attribution
+    class Source
 
     end
 
--- spec/datatypes_spec.rb
+++ spec/datatypes_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe "NumericFieldWithUnits" do
   
   before(:each) do
-    node_string = '{ "baseline": {"numericAmount": 400000, "unitOfMeasurement": "tonnes CO2e"} }'
+    node_string = '{ "fieldname": {"numericAmount": 400000, "unitOfMeasurement": "tonnes CO2e"} }'
     node_json = JSON.parse(node_string)
 
     @node = OpenCarbon::DataTypes::NumericFieldWithUnits.new(node_json)
   end
 
   it "can get the node name" do
-    @node.name.should eq "baseline"
+    @node.name.should eq "fieldname"
   end
 
   it "can get the node amount value" do
@@ -23,6 +23,86 @@ describe "NumericFieldWithUnits" do
 
   it "can format value and units" do
     @node.pretty.should eq "400000 tonnes CO2e"
+  end
+
+end
+
+describe "Baseline" do
+
+  before(:each) do
+    node_string = '{ "baseline": {"year": "2007", "numericAmount": 400000, "unitOfMeasurement": "tonnes CO2e"} }'
+    node_json = JSON.parse(node_string)
+
+    @baseline = OpenCarbon::DataTypes::Baseline.new(node_json)
+  end
+
+  it "can get the year" do
+    @baseline.year.should eq "2007"
+  end
+
+  it "can still get the amount value" do
+    @baseline.value.should eq 400000
+  end
+
+end
+
+describe "Total" do
+
+  before(:each) do
+    node_string = '{ "total": {"numericAmount": 400000, "unitOfMeasurement": "tonnes CO2e"} }'
+    node_json = JSON.parse(node_string)
+
+    @total = OpenCarbon::DataTypes::Total.new(node_json)
+  end
+
+  it "can get the node name" do
+    @total.name.should eq "total"
+  end
+
+end
+
+describe "Scope" do
+
+  before(:each) do
+    node_string = '{ "scope-1": {"numericAmount": 400000, "unitOfMeasurement": "tonnes CO2e"} }'
+    node_json = JSON.parse(node_string)
+
+    @scope = OpenCarbon::DataTypes::Scope.new(node_json)
+  end
+
+  it "can get the node name" do
+    @scope.name.should eq "scope-1"
+  end
+
+end
+
+describe "Scopes" do
+
+  before(:each) do
+    node_string = '{ "scopes": {"scope-1": {"numericAmount": 7794880, "unitOfMeasurement": "tonnes CO2e"}, "scope-2": {"numericAmount": 7131984, "unitOfMeasurement": "tonnes CO2e"} } }'
+    node_json = JSON.parse(node_string)
+
+    @scopes = OpenCarbon::DataTypes::Scopes.new(node_json)
+  end
+
+  it "can get scopes" do
+    @scopes.collection.size.should eq 2
+  end
+
+  it "can get the name for a particular scope" do
+    @scopes.collection.first.name.should eq "scope-1"
+  end
+
+  it "can get a scope by name" do
+    scope = @scopes.find_by_name("scope-2")
+    scope.should_not be_nil
+    scope.value.should eq 7131984
+  end
+
+  it "can handle finding non-existent scopes" do
+    # look for a scope that doesn't exist
+    scope = @scopes.find_by_name("scope-10")
+    scope.should be_nil
   end
 
 end
