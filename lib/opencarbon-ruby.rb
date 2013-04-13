be54657670df93322c5bcb95b472c95d34c92159
Bo Daley <bo@factorypreset.com>
2013-04-12 22:55:27 -0400

    add gem skeleton

---
+++ lib/opencarbon-ruby.rb
@@ -0,0 +1,7 @@
+require "opencarbon-ruby/version"
+
+module Opencarbon
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/opencarbon-ruby/version.rb
@@ -0,0 +1,5 @@
+module Opencarbon
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
